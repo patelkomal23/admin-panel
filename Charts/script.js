5454ee57fc50623c645d9b2a60d5d3e4a10e799b
patelkomal23 <patelkomal2332@gmail.com>
2024-05-04 09:18:05 +0530

    admin panel

---
+++ Charts/script.js
@@ -0,0 +1,40 @@
+
+// $('.menu-icon').on('click', function(){
+//     $(this).toggleClass('active');
+//     $('.slidebar').slideToggle();
+//     // $($(this).attr("for")).show();
+//       $(".icon").toggle()
+//       $("#dashboard").show()
+//   })
+
+  // $(".slidebar").show();
+
+
+  // $(".menu-icon").on("click",function(){
+  //     $(".slidebar").toggle("click",function(){
+  //       $(".main-section").css("width","100%")
+  //     });
+  //     ($(this).attr("for")).show();
+  //     })
+ 
+
+  $(".slider-icon").on("click",function(){
+    $(".sidebar").toggle("click",function(){
+      // $(".contain").css("width","100%")
+      $(".contain>div").css("width","100%")
+    
+    })    
+})
+
+$(".link-name").on("click",function(){
+  $(".sidebar").hide("click",function(){
+    // $(".contain").css("width","100%")
+    // $(".contain>div").css("width","100%")
+  
+  })    
+})
+
+//$(".sidebar").on("click",function(){
+ // $(".sidebar").hide()
+//})
+
